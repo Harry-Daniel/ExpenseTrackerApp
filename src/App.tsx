48cc96279e5c4eb17bf4eddf9e672fd4c0fa8970
Harry-Daniel <hayiborkomla@gmail.com>
2023-10-16 19:31:13 +0000

    Worked on the Expense List Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseList from './components/ExpenseList'
 
+
 function App () {
+  const [Expenses,setExpenses]=useState([
+    
+    {id: 1 , description: 'aaa', amount: 10, category: 'Utility'},
+    {id: 2 , description: 'bbb', amount: 10, category: 'Utility'},
+    {id: 3 , description: 'ccc', amount: 10, category: 'Utility'},
+    {id: 4 , description: 'ddd', amount: 10, category: 'Utility'},
+ ]);
+
+    
   return (
     <div>
-      <ExpenseList/>
+      <ExpenseList onDelete={(id)=>setExpenses(Expenses.filter(e=>e.id!==id))} expenses={Expenses}/>
     </div>
   )
 }
--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+interface Expenses{
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
 
-const ExpenseList = () => {
+interface Props{
+  expenses: Expenses[];
+  onDelete: (id:number)=> void;
+}
+
+const ExpenseList = ({expenses, onDelete}:Props) => {
   return (
-    <div>ExpenseList</div>
+    <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Category</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {expenses.map(expense=> <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
+            <td>
+                <button onClick={()=>onDelete(expense.id)} className="btn btn-outline-danger">
+                  Delete
+                </button>            
+            </td>
+          </tr>)}
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <td>Total</td>
+            <td>GHC {expenses.reduce((accumulator,current)=>current.amount+accumulator,0)}</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tfoot>
+        
+
+    </table>
   )
 }
 
