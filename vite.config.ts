624db3d59694a89321d14aa7560be03b38b58745
Harry-Daniel <hayiborkomla@gmail.com>
2023-10-18 10:26:24 +0000

    Eddited vite.configts and installed vite cli globally

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ExpenseTrackerApp/',
+  build: {
+    outDir: 'dist', // Specify the output directory
+  },
   plugins: [react()]
 })
